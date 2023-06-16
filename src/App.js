bb93b5535dce48ee2ec330fd84851211a9a40de3
Thomas Vovk <127060122+Thomasvovk@users.noreply.github.com>
2023-06-16 10:04:42 -0400

    merged develop

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import Header from "./components/Header/Header";
-import Search from "./components/Search/Search";
 
 export default function App() {
   return (
