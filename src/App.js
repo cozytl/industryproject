a2f0b393a0cb3b514fdbe89e244a6d2de34af161
cozyboystl <103449096+cozytl@users.noreply.github.com>
2023-06-16 11:16:56 -0400

    commit
    a

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import Header from "./components/Header/Header";
+import Proppass from "./components/ProductItem/Proppass";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/id" element={<Proppass />} />
       </Routes>
     </BrowserRouter>
   );
